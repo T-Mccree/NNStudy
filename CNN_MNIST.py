fc089309e18fbf6d732af5af020df6c5b366aed5
T-Mccree <TaoYJ1997@outlook.com>
2020-07-01 21:46:44 +0800

    update

--- CNN_MNIST.py
+++ CNN_MNIST.py
@@ -34,7 +34,7 @@ def create_model():
     model = Sequential()
     model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
+    model.add(Dropout(0.5))
     model.add(Flatten())
     model.add(Dense(units=128, activation='relu'))
     model.add(Dense(units=10, activation='softmax'))
