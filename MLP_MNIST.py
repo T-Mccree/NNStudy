33acc5a68347ce1733f90c7b0c716ec000c8b5ec
T-Mccree <TaoYJ1997@outlook.com>
2020-07-01 16:15:23 +0800

    update

--- MLP_MNIST.py
+++ MLP_MNIST.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+import numpy as np
 
-#导入MNIST数据
+# 导入MNIST数据
+from keras.utils import np_utils
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-#显示四张图
+# 显示四张图
 plt.subplot(221)
 plt.imshow(X_train[0], cmap=plt.get_cmap())
 
@@ -36,3 +39,13 @@ x_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
 x_train_normalize = x_train / 255  # 标准化
 x_test_normalize = x_test / 255
 
+# 进行one-hot编码
+y_trainOneHot = np_utils.to_categorical(y_train)
+y_testOneHot = np_utils.to_categorical(y_test)
+num_class = y_test.shape[1]
+print(num_class)
+
+# 建立模型
+def create_model():
+    model = Sequential()
+    model.add(Dense(units=256, input_dim=784, kernel_initializer='normal', activation='relu'))
